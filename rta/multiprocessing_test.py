afa8a4b182c04b3324a611065df5e8a5c59761d6
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-04-16 16:46:02 +0200

    Grouped-k-folds chosen as x-validation strategy.

---
+++ rta/multiprocessing_test.py
@@ -0,0 +1,10 @@
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        x = p.map(f, range(1000))
+
+x
---
+++ rta/read_in_data.py
@@ -0,0 +1,23 @@
+# import csv
+#
+# with open('data/pure_data.csv', newline='') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     for row in reader:
+#         print(', '.join(row))
+import pandas
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GroupKFold
+from collections import namedtuple
+
+DT = pandas.read_csv('rta/data/pure_data.csv')
+LearnPair = namedtuple('LearnPair', 'training test')
+
+def x_validate(X, folds=10, id = ['id']):
+    gkf = GroupKFold(n_splits=folds)
+    for train, test in gkf.split(X, groups=X[id]):
+        yield LearnPair(X.iloc[train], X.iloc[test])
+
+# Y = list(x_validate(DT))
+# len(Y)
+# Y[0].test
