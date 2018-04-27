1a09d6e702e733ede8ef68552d94765fb8c6aa46
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-04-27 11:32:59 +0200

    Making the filtering procedure work better.

--- rta/preprocessing.py
+++ rta/preprocessing.py
@@ -4,8 +4,8 @@ import pandas as pd
 def preprocess(D, min_runs_no = 5):
     X = D.groupby('id')
     D_stats = pd.DataFrame(dict(runs_no = X.rt.count(),
-                                 median_rt = X.rt.median(),
-                                 median_mass = X.pep_mass.median()))
+                                median_rt = X.rt.median(),
+                                median_mass = X.pep_mass.median()))
     enough_runs = D_stats.index[ D_stats.runs_no >= min_runs_no ]
     D = D.loc[ D.id.isin(enough_runs) ]
     D = pd.merge(D, D_stats, left_on='id', right_index=True)
--- rta/splines/denoising_procedure.py
+++ rta/splines/denoising_procedure.py
@@ -10,12 +10,47 @@ from rta.models.base_model import predict, fitted, coef, residuals
 from rta.models import spline
 from rta.models.plot import plot
 
+formula = "rt_median_distance ~ bs(rt, df=40, degree=2, lower_bound=0, upper_bound=200, include_intercept=True) - 1"
 
 DF = preprocess(D, min_runs_no = 2)
-for g, data in DF.groupby('run'):
-    pass
 
-formula = "rt_median_distance ~ bs(rt, df=40, degree=2, lower_bound=0, upper_bound=200, include_intercept=True) - 1"
-model = spline(data, formula)
+# Removing the peptides that have their RT equal to the median.
+# TODO: think if these points cannot be directly modelled.
+# and what if we start moving them around?
+DF = DF[DF.rt_median_distance != 0]
+
+models = { g: spline(data, formula) for g, data in DF.groupby('run')}
+
+g = 1
+model = models[1]
+
+for run_no, data in DF.groupby('run'):
+    # Fitting the spline
+    model = spline(data, formula)
+
+    # Fitting the Gaussian mixture model
+    res = residuals(model).reshape((-1,1))
+    gmm = mixture.GaussianMixture(n_components=2,
+                                  covariance_type='full').fit(res)
+    signal_idx, noise_idx = np.argsort(gmm.covariances_.ravel())
+    sn = {signal_idx: 'signal', noise_idx: 'noise'}
+
+    gmm.predict(res)
+
+
+
 %matplotlib
-plot(model)
+plot(models[1])
+
+
+
+
+# %matplotlib
+# divmod(6,5)
+# for g, model in models.items():
+#     div, mod = divmod(g, 5)
+#     plt.subplot(2, div + 1, mod)
+#     plot(model)
+#
+# plt.subplot(2, 2, 2)
+# plot(models[2])
