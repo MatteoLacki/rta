b7c7fb6d96dee192fad1615c63536b5a76ff6b59
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-05-02 11:47:05 +0200

    Getting into clustering.

--- rta/splines/denoising_procedure.py
+++ rta/splines/denoising_procedure.py
@@ -12,7 +12,7 @@ from rta.models.base_model import predict, fitted, coef, residuals
 from rta.models import spline
 from rta.models.plot import plot, plot_curve
 
-
+# better use natural splines
 formula = "rt_median_distance ~ bs(rt, df=40, degree=2, lower_bound=0, upper_bound=200, include_intercept=True) - 1"
 DF = preprocess(D, min_runs_no = 2)
 
@@ -20,13 +20,16 @@ DF = preprocess(D, min_runs_no = 2)
 # TODO: think if these points cannot be directly modelled.
 # and what if we start moving them around?
 DF = DF[DF.rt_median_distance != 0]
-DF['signal'] = 'signal' # guardian
+
+# All points are 'signal' before denoising: guardians
+DF['signal'] = 'signal'
+
+# Division by run number
 runs = list(data for _, data in DF.groupby('run'))
 
-
-
-def denoise(runs, formula):
-    """Remove noise in grouped data.
+# data = runs[0]
+def denoise_and_align(runs, formula, refit=True):
+    """Remove noise in grouped data and align the retention times.
 
     Updates the 'signal' column in the original data chunks.
     """
@@ -35,22 +38,41 @@ def denoise(runs, formula):
         # Fitting the spline
         signal_indices = data.signal == 'signal'
         model = spline(data[signal_indices], formula)
+
         # Fitting the Gaussian mixture model
         res = residuals(model).reshape((-1,1))
         gmm = mixture.GaussianMixture(n_components=2,
                                       covariance_type='full').fit(res)
+
+        # The signal is the cluster with smaller variance
         signal_idx, noise_idx = np.argsort(gmm.covariances_.ravel())
         sn = {signal_idx: 'signal', noise_idx: 'noise'}
-        # appending to the data set
+
+        # Retaggin some signal tags to noise tags
         data.loc[signal_indices, 'signal'] = [ sn[i] for i in gmm.predict(res)]
-        model = spline(data[data.signal == 'signal'], formula)
+
+        # Refitting the spline only to the signal peptides
+        if refit:
+            model = spline(data[data.signal == 'signal'], formula)
+
+        # Calculating the new retention times
+        data.loc[signal_indices, 'rt_aligned'] = data[data.signal == 'signal'].rt - fitted(model)
+
+        # Coup de grace!
         yield model, data
 
 
 %%time
-models = list(denoise(runs, formula))
+models = list(denoise_and_align(runs, formula))
+
+
 d = models[0][1]
 m = models[0][0]
+
+%matplotlib
+plt.plot(d[d.signal == 'signal'].rt, d[d.signal == 'signal'].rt_aligned)
+plt.hist(residuals(m))
+
 
 %matplotlib
 plt.scatter(d.rt,
@@ -59,6 +81,8 @@ plt.scatter(d.rt,
 plot_curve(m, c='blue', linewidth=4)
 %matplotlib
 plot(models[1][0])
+
+
 
 
 
