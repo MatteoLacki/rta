2b46fc00d6a30a64d185dbbf519696685624d07a
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-05-04 17:41:49 +0200

    running the graph generation for weekend.

--- rta/scripts/find_neighbour_graph.py
+++ rta/scripts/find_neighbour_graph.py
@@ -1,24 +1,10 @@
-import bisect
 from collections import namedtuple, Counter, defaultdict
 import numpy as np
 import pandas as pd
 import intervaltree as it
 import networkx as nx
 from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-
-D = pd.read_csv('rta/data/denoised_data.csv')
-D_signal = D[D.signal == 'signal']
-peptide_grouping = D_signal.groupby('id')
-signal_runs_no = peptide_grouping.rt.count()
-
-min_runs_no = 2
-D_signal_min_cnts = D_signal[D_signal.id.isin(signal_runs_no[signal_runs_no >= min_runs_no].index)]
-eps = np.finfo(float).eps
-
-
-
-
+import pickle
 
 class Multivariate_Tree(object):
     def __init__(self, data, id_column_name, column_names,
@@ -45,11 +31,6 @@ class Multivariate_Tree(object):
         return {n : self.peps[n][x] for n in self.names}
 
 
-
-
-# MT = Multivariate_Tree(D_signal_min_cnts, 'id', ('rt_aligned', 'dt', 'le_mass'))
-
-
 X, y = make_blobs(n_samples = 3000,
                   n_features = 3,
                   centers = 30)
@@ -60,33 +41,6 @@ X['id'] = y
 
 MT = Multivariate_Tree(X, 'id', ('rt_aligned', 'dt', 'le_mass'))
 
-
-
-
-
-# class T(object):
-#     def __init__(self, mt):
-#         self.tree = mt.tree
-#         self.names = mt.names
-#         self.peps = mt.peps
-#
-#     def get(self, **x):
-#         return set.intersection(*(set(e.data for e in self.tree[k][v])
-#                                   for k, v in x.items() ))
-#
-#     def __getitem__(self, x):
-#         return {n : self.peps[n][x] for n in self.names}
-#
-# t = T(MT)
-# x = t.get(dt = 110).pop()
-# t[x]
-#
-# t.get(rt_aligned=it.Interval(100, 101),
-#       dt=it.Interval(100, 101))
-
-
-
-# peptides = set(D_signal_min_cnts.id)
 peptides = set(X.id)
 G = nx.Graph()
 
@@ -98,5 +52,6 @@ while len(peptides) > 0:
         G.add_node(other_pep)
         G.add_edge(pep, other_pep)
 
-%matplotlib
-nx.draw(G)
+
+with open('rta/data/graph.pickle', 'wb') as h:
+    pickle.dump(G, h)
