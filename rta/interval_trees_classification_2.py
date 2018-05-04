789584f22196c1dfb87562bc2e5f99144a83594d
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-05-04 17:16:55 +0200

    Proofchecking the graph making.

--- rta/interval_trees_classification_2.py
+++ rta/interval_trees_classification_2.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import intervaltree as it
 import networkx as nx
-from sklearn.datasets import make_blobs
 
 
 D = pd.read_csv('rta/data/denoised_data.csv')
@@ -15,7 +14,6 @@ signal_runs_no = peptide_grouping.rt.count()
 min_runs_no = 2
 D_signal_min_cnts = D_signal[D_signal.id.isin(signal_runs_no[signal_runs_no >= min_runs_no].index)]
 eps = np.finfo(float).eps
-
 
 
 
@@ -43,49 +41,3 @@ class Multivariate_Tree(object):
 
     def __getitem__(self, x):
         return {n : self.peps[n][x] for n in self.names}
-
-
-
-
-# MT = Multivariate_Tree(D_signal_min_cnts, 'id', ('rt_aligned', 'dt', 'le_mass'))
-
-
-make_blobs
-
-
-MT.get(dt = 110, le_mass=1000)
-pep = MT.get(dt = 110, le_mass=1000).pop()
-MT[pep]
-
-# class T(object):
-#     def __init__(self, mt):
-#         self.tree = mt.tree
-#         self.names = mt.names
-#         self.peps = mt.peps
-#
-#     def get(self, **x):
-#         return set.intersection(*(set(e.data for e in self.tree[k][v])
-#                                   for k, v in x.items() ))
-#
-#     def __getitem__(self, x):
-#         return {n : self.peps[n][x] for n in self.names}
-#
-# t = T(MT)
-# x = t.get(dt = 110).pop()
-# t[x]
-#
-# t.get(rt_aligned=it.Interval(100, 101),
-#       dt=it.Interval(100, 101))
-
-
-
-peptides = set(D_signal_min_cnts.id)
-G = nx.Graph()
-
-while len(peptides) > 0:
-    pep = peptides.pop()
-    G.add_node(pep)
-    for other_pep in MT.get(**{k: it.Interval(v[0], v[1])
-                               for k, v in MT[pep].items()}):
-        G.add_node(other_pep)
-        G.add_edge(pep, other_pep)
