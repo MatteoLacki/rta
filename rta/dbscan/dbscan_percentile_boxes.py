5738ef16ee356eabe02cd30f3c04419dc6134f4e
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-05-14 11:09:26 +0200

    Pandas is shit.

--- rta/dbscan/dbscan_percentile_boxes.py
+++ rta/dbscan/dbscan_percentile_boxes.py
@@ -21,8 +21,6 @@ DF = preprocess(D, min_runs_no = 2)
 # DF = DF[DF.rt_median_distance != 0]
 
 
-len(set(DF.id))
-
 # All points are 'signal' before denoising: guardians
 DF['signal'] = 'signal'
 # Division by run number
@@ -32,7 +30,6 @@ models = list(denoise_and_align(runs, formula))
 
 # resemble all the data sets
 DF_2 = pd.concat(d for m, d in models)
-DF_2.head()
 
 # The noise seems to be confirmed by the analysis of the drift times.
 [{g: np.median(np.abs(x.dt_median_distance))
@@ -47,74 +44,79 @@ DF_2.head()
 
 # Calculate the spaces in different directions for signal points
 DF_2_signal = DF_2[DF_2.signal == 'signal']
-data = DF_2_signal
+# data = DF_2_signal
+# DBSCAN_args = {}
+def normalize_and_cluster(data_and_percentiles):
+    percentiles, data, DBSCAN_args = data_and_percentiles
+    NORMALIZED = data.copy()
+    for col, percentile in zip(['le_mass', 'rt_aligned', 'dt'],
+                               percentiles):
+        NORMALIZED[col] = (data[col] - data[col].min()) / percentile
+    DBSCAN = cluster.DBSCAN(**DBSCAN_args)
+    dbscan_res = DBSCAN.fit(NORMALIZED)
+    return dbscan_res
 
 
 def cluster_percentile_test(data,
-                            percentiles = np.arange(10,101,10),
-                            colnames = ['le_mass', 'rt_aligned', 'dt'],
-                            workers_cnt = 15,
-                            DBSCAN_args**):
+                            percentiles     = np.arange(10,101,10),
+                            colnames        = ['le_mass', 'rt_aligned', 'dt'],
+                            workers_cnt     = 15,
+                            eps             = 1.0,
+                            min_samples     = 3,
+                            metric          = 'chebyshev',
+                            **DBSCAN_args):
     """Run the test of percentile clustering."""
     X = data.groupby('id')
-    D_stats = pd.DataFrame(dict(runs_no_aligned         = X.rt.count(),
-                                rt_aligned_median       = X.rt.median(),
-                                rt_aligned_min          = X.rt.min(),
-                                rt_aligned_max          = X.rt.max(),
-                                rt_aligned_max_space    = X.rt_aligned.aggregate(max_space),
-                                pep_mass_max__space     = X.pep_mass.aggregate(max_space),
-                                le_mass_max_space       = X.le_mass.aggregate(max_space),
-                                le_mass_aligned_min     = X.le_mass.min(),
-                                le_mass_aligned_max     = X.le_mass.max(),
-                                dt_max_space            = X.dt.aggregate(max_space),
-                                dt_aligned_min          = X.dt.min(),
-                                dt_aligned_max          = X.dt.max()))
+    DBSCAN_args.update(dict(metric      = metric,
+                            eps         = eps,
+                            min_samples = min_samples))
+    D_stats = pd.DataFrame(dict(runs_no_aligned      = X.rt.count(),
+                                rt_aligned_median    = X.rt.median(),
+                                rt_aligned_min       = X.rt.min(),
+                                rt_aligned_max       = X.rt.max(),
+                                rt_aligned_max_space = X.rt_aligned.aggregate(max_space),
+                                pep_mass_max__space  = X.pep_mass.aggregate(max_space),
+                                le_mass_max_space    = X.le_mass.aggregate(max_space),
+                                le_mass_aligned_min  = X.le_mass.min(),
+                                le_mass_aligned_max  = X.le_mass.max(),
+                                dt_max_space         = X.dt.aggregate(max_space),
+                                dt_aligned_min       = X.dt.min(),
+                                dt_aligned_max       = X.dt.max()))
 
     # Getting the percentiles of selected features
     percentiles = {col: np.percentile(D_stats[col + '_max_space'], percentiles)
                    for col in colnames}
-
-    DBSCAN = cluster.DBSCAN(**DBSCAN_args)
     CLUST_ME = DF_2_signal[colnames]
-
-    def __normalize_and_cluster(data_and_percentiles):
-        percentiles, data = data_and_percentiles
-        NORMALIZED = data.copy()
-        for col, percentile in zip(['le_mass', 'rt_aligned', 'dt'],
-                                   percentiles):
-            NORMALIZED[col] = (data[col] - data[col].min()) / percentile
-        dbscan_res = DBSCAN.fit(NORMALIZED)
-        return dbscan_res
-
     data_and_percentiles = zip(product(*[percentiles[name] for name in colnames]),
-                               repeat(CLUST_ME))
+                               repeat(CLUST_ME),
+                               repeat(DBSCAN_args))
 
     with Pool(workers_cnt) as workers:
-        dbscans = workers.map(__normalize_and_cluster,
+        dbscans = workers.map(normalize_and_cluster,
                               data_and_percentiles)
 
     return dbscans
 
-
-Counter(dbscans[0].labels_)
-
+dbscans = cluster_percentile_test(DF_2_signal)
 
 
-dbscan_stats = Counter(dbscan_res.labels_)
+
+# Deriving the criterion for the
+dbscan = dbscans[100]
+DF_2_signal = DF_2_signal.assign(clustering_label = dbscan.labels_)
+
+X = DF_2_signal.groupby('id')
+X.clustering_label.transform(lambda x: np.sum(x==-1))
+X.clustering_label.apply(lambda x: np.sum(x==-1))
 
 
-# how many points to cluster?
-len(dbscan_res.labels_)
+clustering_stats = pd.DataFrame(dict(noise_cnt = X.))
 
-# how many clusters
-len(set(dbscan_stats))
 
-# how many points per cluster in different clusters in general?
-len(set(dbscan_stats.values()))
 
-# run tests
-cluster_percentile_test(DF_2_signal,
-                        eps = 1.0,
-                        min_samples = 3,
-                        metric = 'chebyshev')
+DF_2_signal.noise_points
 
+(DF_2_signal.clustering_label == -1).group_by('idx')
+.sum()
+
+X.clustering_label
