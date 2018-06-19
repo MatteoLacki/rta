01e59aabf6d8b72bd2259f3bb25457d674cda893
matteo <matteo.lacki@gmail.com>
2018-06-19 13:44:43 +0200

    Finally, fold properly appear in the main DF.

--- rta/xvalidation/cross_validation.py
+++ rta/xvalidation/cross_validation.py
@@ -19,42 +19,30 @@ from rta.preprocessing import preprocess
 from rta.xvalidation.grouped_k_folds import grouped_K_folds
 from rta.xvalidation.filters import filter_K_foldable
 
-data = pd.read_csv(data_folder("one_run_5_folds.csv"))
-chunks_no = 20
-s_model = SQSpline()
-s_model.df_2_data(data, 'rt', 'rt_median_distance')
-s_model.fit(chunks_no=chunks_no)
+# data = pd.read_csv(data_folder("one_run_5_folds.csv"))
+# chunks_no = 20
+# s_model = SQSpline()
+# s_model.df_2_data(data, 'rt', 'rt_median_distance')
+# s_model.fit(chunks_no=chunks_no)
 
+# optmize the creation of these data-sets
 annotated, unlabelled = big_data()
 annotated, annotated_stats = preprocess(annotated, min_runs_no = 2)
 
+# this should be part of preprocessing
 folds_no = K = 5
-
-# only 50 ms
 annotated_cv, annotated_stats_cv, run_counts = filter_K_foldable(annotated, annotated_stats, K)
+annotated_cv, annotated_stats_cv, run_counts = annotated_cv.copy(), annotated_stats_cv.copy(), run_counts.copy()
+annotated_cv_slim = annotated_cv[['id', 'run', 'rt', 'rt_median_distance']]
+annotated_cv_slim.reset_index(inplace=True, drop=True)
 
-# build up the groups
-annotated_stats_cv
-
-# so now, we have to build up space for the folds indices
-folds = np.zeros(len(annotated_stats_cv))
 
 
 # TODO: we do not need that: replace it with simple for with if condition.
 # what are the numbers of peptides in each foldable group?
 peptide_counts_in_runs = dict(((runs, cnt) for runs, cnt in run_counts.items() if cnt >= K))
 
-# within each "runs" group, do the assignment to folds
-annotated_stats_cv.groupby('runs')
-
-
-# this can be very well done based on the 'run_counts'
-
-
-
-
-def test(x, K):
-    N = len(x)
+def test(N, K):
     groups = np.full((N,), 0)
     # produce an array of K numbers repeated 
     N_div_K = N // K
@@ -68,104 +56,24 @@ def test(x, K):
     shuffle(groups)
     return groups
 
-folds = annotated_stats_cv.groupby('runs').runs_no.transform(test, K=K)
+# so now, we have to build up space for the folds indices
 
+def assign_peptides_to_folds(peptides_cnt):
+    folds = np.zeros(peptides_cnt, dtype=np.int8)
+    s = e = 0
+    for cnt in peptide_counts_in_runs.values():
+        e += cnt
+        folds[s:e] = test(cnt, K)
+        s = e
+    return folds
 
-pd.merge(annotated_cv, 
-         pd.DataFrame(folds), left_on="id", right_index=True)
+# this divides the proteins into folds
+annotated_stats_cv['fold'] = assign_peptides_to_folds(len(annotated_stats_cv))
 
-
-# we have filled the folds with proper values
-# now, we have to order the annotated_cv based on the values of this vector.
-# this can be done wihout appending the function
-
-# the order is wrong
-
-
-
-
-
-# then, we will have to scan the df for the order of these things and pass the views of
-# the data to fitting procedures
-
-
-
-
-
-# will it be faster if we change the whole peptide_id to integers?
-annotated_cv.id = annotated_cv.id.astype('category')
-annotated_cv.id.dtypes.categories
-
-folds = annotated_cv.groupby('runs').rt.transform(grouped_K_folds, 
-                                                  K=K).astype(np.int8)
-annotated_cv = annotated_cv.assign(fold=folds)
-
-# implement the whole procedure without data copying.
-# simply shuffle the whole DF and then pass the indices marking the 
-# beginnings and ends of the data.
-
-# maybe also assure that the data is stored in a continguous way.
-# maybe the data should consist of two float entries?
-
-# there should be some initial ordering of the data
-annotated_cv.sort_values(by=["run", "rt"], inplace=True)
-annotated_cv.run
-annotated_cv.rt
-
-x = annotated_cv.id.copy()
-ids = np.unique(x)
-N = len(ids)
-N_div_K = N // K
-N_mod_K = N % K
-groups = np.full((N,), 0)
-
-
-for i in range(1, K):
-    groups[ i * N_div_K : (i+1) * N_div_K ] = i
-if N_mod_K: 
-    # we decide at random (sampling without repetition)
-    # to which groups we should assign the remaining free indices.
-    group_tags = list(range(K))
-    shuffle(group_tags)         # random permutation of groups
-    groups[-N_mod_K:] = group_tags[0:N_mod_K] # assigning the remainder
-shuffle(groups) #???
-
-folds_no = K = 10
-grouped_K_folds(annotated_cv.id, 10)
-N = len(annotated_stats)
-
-
-
-
-
-
-def random_fold_split(N, K):
-    N_div_K = N // K
-    N_mod_K = N % K
-    enlarged_groups = npr.choice(K, N_mod_K, replace=True)
-    yield 0
-    for 
-
-
-
-
-
-
-# the filtering should be part of the statistics procedure
-x = np.arange(N)
-npr.shuffle(x)
-
-# we have to divide the above vector to smaller chunks
-# we have to map numbers to individual ids. 
-# but that's easy, since it's simply having to have an array of peptide-ids.
-
-
-annotated_stats.sort_values('runs', inplace=True)
-Count(annotated_stats.runs)
-
-def get_shuffling(x):
-    return shuffle(np.arange(len(x)))
-
-annotated_stats.groupby('runs').transform(get())
-
+# now, we have to divide proteins in runs into groups
+annotated_cv_slim = pd.merge(annotated_cv_slim, 
+                             pd.DataFrame(annotated_stats_cv.fold),
+                             left_on='id',
+                             right_index=True)
+annotated_cv_slim.head()
 
