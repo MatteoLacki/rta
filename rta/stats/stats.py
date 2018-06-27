Something must be wrong in the calculation.

rta/stats/stats.py
@@ -111,18 +111,18 @@ def fold_similarity(data,
         distances_agg (pandas.DataFrame): distances aggregated over folds and runs.
     """
     run_data = data.groupby(run_name)
-    r_perc = dict(run_data.rt.apply(np.percentile, q=q))
+    r_perc = dict(run_data.rt.apply(np.percentile, q=quantiles))
 
     def iter_distances_from_fold_perc_2_real_perc():
         for (run, fold), rf_d in data.groupby([run_name, fold_name]):
             out = np.array((run, fold))
-            rf_perc = np.percentile(rf_d.rt, q=q)
+            rf_perc = np.percentile(rf_d.rt, q=quantiles)
             out = np.append(out, np.abs(rf_perc - r_perc[run]))
             yield out
 
     distances = pd.DataFrame(iter_distances_from_fold_perc_2_real_perc())
     distances[[0,1]] = distances[[0,1]].astype(int)
-    cols = [run_name, fold_name] + list(q)
+    cols = [run_name, fold_name] + list(quantiles)
     distances.columns = cols
     distances.set_index([run_name, fold_name], inplace=True)
     distances_run = pd.DataFrame(distances.groupby(run_name).describe())

rta/xvalidation/stratifications_folds.py
@@ -74,7 +74,7 @@ def iter_shuffled_tenzer_folds(run_cnts, folds_no=10):
     with repetitions for all consecutive strata.
 
     Args:
-        runs_cnts (pandas.core.series.Series): the counts of occurences of peptides in different run groups.
+        runs_cnts (iterable): the counts of occurences of peptides in different run groups.
         folds_no (int): the number of folds the data will be divided into.
 
     Yield:
@@ -101,7 +101,7 @@ def tenzer_folds(run_cnts, folds_no, shuffle=False):
     The 'run_cnts' induce the order of appearance of folds.
 
     Args:
-        runs_cnts (pandas.core.series.Series): the counts of occurences of peptides in different run groups.
+        runs_cnts (iterable): the counts of occurences of peptides in different run groups.
         folds_no (int): the number of folds the data will be divided into.
 
     Return:
@@ -128,7 +128,7 @@ def randomized_tenzer_folds(run_cnts, folds_no, shuffle=True):
     The 'run_cnts' induce the order of appearance of folds.
 
     Args:
-        runs_cnts (pandas.core.series.Series): the counts of occurences of peptides in different run groups.
+        runs_cnts (iterable): the counts of occurences of peptides in different run groups.
         folds_no (int): the number of folds the data will be divided into.
 
     Return:
@@ -137,7 +137,45 @@ def randomized_tenzer_folds(run_cnts, folds_no, shuffle=True):
     return tenzer_folds(run_cnts, folds_no, True)
 
 
-#TODO: this should not need the computation of runs_cnts
+def no_runs_strata_tenzer_folds(run_cnts, folds_no):
+    """Create Stefan Tenzer folds without strata.
+
+    Divide points into different folds neglecting their appearance 
+    within different groups. The 'natural randomness' of the data points
+    is used.
+
+    Args:
+        runs_cnts (iterable): the counts of occurences of peptides in different run groups.
+        folds_no (int): the number of folds the data will be divided into.
+
+    Return:
+        out (np.array of ints): the folds prescription for individual peptide groups.
+    """
+    return np.fromiter(cycle(range(folds_no)),
+                       count=sum(run_cnts),
+                       dtype=np.int8)
+
+
+def no_runs_strata_randomized_tenzer_folds(run_cnts, folds_no):
+    """Create Stefan Tenzer folds without strata.
+
+    Divide points into different folds neglecting their appearance 
+    within different groups. The 'natural randomness' of the data points
+    is used.
+
+    Args:
+        runs_cnts (iterable): the counts of occurences of peptides in different run groups.
+        folds_no (int): the number of folds the data will be divided into.
+
+    Return:
+        out (np.array of ints): the folds prescription for individual peptide groups.
+    """
+    folds = list(range(folds_no))
+    return np.fromiter(shuffled_folds(folds),
+                       count=sum(run_cnts),
+                       dtype=np.int8)
+
+
 def replacement_sampled_folds(run_cnts, folds_no=10):
     """Draw folds truly at random.
 