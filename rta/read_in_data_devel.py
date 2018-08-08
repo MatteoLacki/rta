de64164baa69cda57bc842e9daea61019d8286c9
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-08-08 14:22:56 +0200

    Porting to R completed. Need to link it with the rest of the code in something meaningful for Ute.

--- rta/read_in_data_devel.py
+++ rta/read_in_data_devel.py
@@ -24,34 +24,51 @@ from rta.config       import *
 project = "Proj__15272392369260_8293106731954075_100_1"
 data    = retrieve_data(password  = password,
                         user      = user,
+                        ip        = ip,
                         project   = project,
                         verbose   = True)
 
-# data that has not been given any sequence.
-U = data.loc[data.sequence.isna(), col_names_unlabelled].copy()
 
-# all the identified peptides.
-L = data.dropna(subset = ['sequence']).copy()
+def split(L, cond):
+    wrong = L[ L.index.isin(cond[cond  > 1].index) ]
+    good  = L[ L.index.isin(cond[cond == 1].index) ]
+    return good, wrong
+
+def filter_peptides_with_unique_types(data, return_filtered = True):
+    """Filter out peptides that appear in more than one type per run.
+
+    Also, find peptides that appear with in different types across different runs.
+
+    Args:
+        data (pd.DataFrame):       Mass Project data.
+        return_filtered (logical): Return the filtered out peptides too.
+    Returns:
+        tuple : filtered peptides and unlabelled petides. Optionally, filtered out peptides, too.
+    """
+    # data that has not been given any sequence.
+    U = data.loc[data.sequence.isna(), col_names_unlabelled].copy()
+    # all the identified peptides.
+    L = data.dropna(subset = ['sequence']).copy()
+    L.set_index(['sequence', 'modification', 'run', 'type'], inplace = True)
+    L.sort_index(inplace = True)
+    # filter peptides that appear multpile time with the same type in the same run
+    id_run_type_cnt = L.groupby(L.index).size()
+    L, non_unique_id_run_type = split(L, id_run_type_cnt)
+    # filter peptides identified in more than one type per run
+    L.reset_index(level = ['type'], inplace = True)
+    L.sort_index(inplace = True)
+    types_per_id_run = L.groupby(L.index).size()
+    L, non_unique_type_per_id_run = split(L, types_per_id_run)
+    # filter out peptides identified with different types in different runs
+    L.reset_index(level = 'run', inplace = True)
+    L.sort_index(inplace = True)
+    diff_types_diff_runs = L.groupby(L.index).type.nunique()
+    L, diff_types_in_diff_runs = split(L, diff_types_diff_runs)
+    if return_filtered:
+        return L, U, non_unique_id_run_type, non_unique_type_per_id_run, diff_types_in_diff_runs
+    else: # HAHAHA, LU decomposition - FU!!!
+        return L, U
+
+L, U = filter_peptides_with_unique_types(data, False)
+
 # L['id'] = L['sequence'] + " " + L['modification'].astype(str) # old way
-L.set_index(['sequence', 'modification', 'run', 'type'], inplace = True)
-
-# filter out peptides that have more than one instance while conditioning on id, run and type.
-# eliminate multiple peptides in the same run with the same id.
-L.sort_index(inplace = True)
-id_run_type_cnt = L.groupby(L.index).size()
-non_unique_id_run_type = L[ L.index.isin(id_run_type_cnt[id_run_type_cnt > 1].index) ]
-L = L[ L.index.isin(id_run_type_cnt[id_run_type_cnt == 1].index) ]
-
-# filter peptides identified in more than one type per run
-L.reset_index(level = ['type'], inplace = True)
-types_per_id_run = L.groupby(L.index).size()
-non_unique_type_per_id_run = L[ L.index.isin(types_per_id_run[types_per_id_run > 1].index) ]
-L = L[ L.index.isin(types_per_id_run[types_per_id_run == 1].index) ]
-
-# filter out peptides identified with different types in different runs
-L.reset_index(level = 'run', inplace = True)
-
-diff_types_diff_runs = L.groupby(L.index).type.nunique()
-peps_diff_types_in_diff_runs = L[ L.index.isin(diff_types_diff_runs[diff_types_diff_runs > 1].index)]
-L = L[ L.index.isin(diff_types_diff_runs[diff_types_diff_runs == 1].index)]
-
