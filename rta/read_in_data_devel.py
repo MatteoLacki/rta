e145534c97f06856cf63ecec3401e83063df4385
MatteoLacki <matteo.lacki@uni-mainz.de>
2018-08-08 14:00:37 +0200

    Ported R code entirely to Python.

--- rta/read_in_data_devel.py
+++ rta/read_in_data_devel.py
@@ -27,27 +27,31 @@ data    = retrieve_data(password  = password,
                         project   = project,
                         verbose   = True)
 
-# two different ways to split a DataFrame.
 # data that has not been given any sequence.
-unlabelled = data.loc[data.sequence.isna(),
-                      col_names_unlabelled].copy()
+U = data.loc[data.sequence.isna(), col_names_unlabelled].copy()
+
 # all the identified peptides.
-labelled   = data.dropna(subset = ['sequence']).copy()
-labelled['id'] = labelled['sequence'] + " " + labelled['modification'].astype(str)
+L = data.dropna(subset = ['sequence']).copy()
+# L['id'] = L['sequence'] + " " + L['modification'].astype(str) # old way
+L.set_index(['sequence', 'modification', 'run', 'type'], inplace = True)
 
 # filter out peptides that have more than one instance while conditioning on id, run and type.
-id_run_type_cnt = labelled.groupby(['id', 'run', 'type']).id.count()
+# eliminate multiple peptides in the same run with the same id.
+L.sort_index(inplace = True)
+id_run_type_cnt = L.groupby(L.index).size()
+non_unique_id_run_type = L[ L.index.isin(id_run_type_cnt[id_run_type_cnt > 1].index) ]
+L = L[ L.index.isin(id_run_type_cnt[id_run_type_cnt == 1].index) ]
 
+# filter peptides identified in more than one type per run
+L.reset_index(level = ['type'], inplace = True)
+types_per_id_run = L.groupby(L.index).size()
+non_unique_type_per_id_run = L[ L.index.isin(types_per_id_run[types_per_id_run > 1].index) ]
+L = L[ L.index.isin(types_per_id_run[types_per_id_run == 1].index) ]
 
-len(id_run_type_cnt[id_run_type_cnt > 1])
+# filter out peptides identified with different types in different runs
+L.reset_index(level = 'run', inplace = True)
 
-labelled.merge(
-    pd.DataFrame(id_run_type_cnt[id_run_type_cnt == 1]),
-    on = ('id', 'run', 'type'))
+diff_types_diff_runs = L.groupby(L.index).type.nunique()
+peps_diff_types_in_diff_runs = L[ L.index.isin(diff_types_diff_runs[diff_types_diff_runs > 1].index)]
+L = L[ L.index.isin(diff_types_diff_runs[diff_types_diff_runs == 1].index)]
 
-
-pd.DataFrame(id_run_type_cnt[id_run_type_cnt == 1]).index
-
-pd.DataFrame(id_run_type_cnt[id_run_type_cnt == 1]).join(
-
-    )
