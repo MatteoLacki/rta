e535b839398f7d8065a68c9706ebf3728b20fa57
matteo <matteo.lacki@gmail.com>
2019-03-20 13:47:47 +0100

    Time to go to work.

--- rta/dev_prepare_data_4_clusters.py
+++ rta/dev_prepare_data_4_clusters.py
@@ -1,6 +1,7 @@
 %load_ext autoreload
 %autoreload 2
 
+from collections import Counter
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -20,7 +21,7 @@ A = pd.read_msgpack(data/"annotated_all.msg")
 U = pd.read_msgpack(data/'unlabelled_all.msg')
 min_runs_per_id = 5 # how many times should peptides appear out of 10 times
 
-# get alignmets
+## get alignmets
 D, stats, pddra, pepts_per_run = preprocess(A, min_runs_per_id)
 D = D.reset_index()
 D['rt_me'] = cond_medians(D.rt, D.id)
@@ -28,7 +29,8 @@ runs = np.unique(D.run)
 B = BigModel({r: RollingMedian() for r in runs})
 B.fit(D.rt.values, D.rt_me.values, D.run.values)
 D['rta'] = B(D.rt, D.run)
-# filtering out the identification that are too far from median
+
+## filtering out the identification that are too far from median
 D['rta_me'] = cond_medians(D.rta, D.id)
 D['rta_d'] = D.rta_me - D.rta
 minmax_rta_d_run = D.groupby('run').rta_d.apply(robust_chebyshev_interval).apply(pd.Series)
@@ -48,8 +50,27 @@ U['rta'] = B(U.rt, U.run)
 U['who'] = 'no_id'
 U = U.append(angry, sort=False)
 
-# saving
+## getting rid of the dt problem
+# dt = A.dt
+# dt_me = cond_medians(A.dt, A.id)
+# plot_distances_to_reference(dt, dt_me, A.run, s=1)
+
+X = A[['id', 'charge']].set_index('id')
+W = X.groupby('id').charge.nunique()
+W = pd.DataFrame(W)
+W.columns = ['q_cnt']
+A = A.join(W, on='id')
+multicharged = A.loc[A.q_cnt > 1,] # 9 064
+A = A.loc[A.q_cnt == 1,] # 269 291
+
+dt = A.dt
+dt_me = cond_medians(A.dt, A.id)
+plot_distances_to_reference(dt, dt_me, A.run, s=1)
+
+
+
+## saving
 A.to_msgpack(data/"A.msg")
 U.to_msgpack(data/"U.msg")
 angry.to_msgpack(data/"angry.msg")
-
+multicharged.to_msgpack(data/"multi_q.msg")
