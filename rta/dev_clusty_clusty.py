f054775c96eb33504eece1ba693d057b3554e13e
matteo <matteo.lacki@gmail.com>
2019-03-21 16:44:02 +0100

    Starting developping the clustering procedures.

--- rta/dev_clusty_clusty.py
+++ rta/dev_clusty_clusty.py
@@ -4,19 +4,26 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
 
-from rta.reference import cond_medians
-from rta.filters.angry import is_angry
-from rta.models.big import BigModel
-from rta.models.rolling_median import RollingMedianSpline
-from rta.plot.runs import plot_distances_to_reference
 
 data = Path("~/Projects/rta/data").expanduser()
+A = pd.read_msgpack(data/"A.msg")
 D = pd.read_msgpack(data/"D.msg")
 U = pd.read_msgpack(data/"U.msg")
 
-DW = D[['id','run','rt']].pivot(index='id', columns='run', values='rt')
-sum(DW.isnull())
+
+AW = A[['id','run','rt']].pivot(index='id', columns='run', values='rt')
+100 * AW.isnull().values.sum() / np.prod(AW.shape) # 80% of slots are free!
+
+# plt.hexbin(A.mass, A.dt, gridsize=200)
+# plt.show()
+# plt.hexbin(A.mass, A.rt, gridsize=200)
+# plt.show()
+# plt.hexbin(A.dt, A.rt, gridsize=200)
+# plt.show()
+
 
 from scipy.spatial import KDTree
 
@@ -31,3 +38,5 @@ from math import inf
 d, i = kd.query(d1, k=10, p=inf, distance_upper_bound=1)
 
 # OK, this is not yet meaningful, but gets there.
+
+
