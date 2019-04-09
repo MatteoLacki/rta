9bba03c0239ffa1ab80eeaf49e2dd2a833ae265a
matteo <matteo.lacki@gmail.com>
2019-04-09 17:25:27 +0200

    Need to add these stupid left and right intervals and maybe better make all in C.

---
+++ rta/dev_mass_boxes.py
@@ -0,0 +1,210 @@
+from bisect import bisect
+import numpy as np
+
+
+class LightweightSpectrum(object):
+    def __init__(self, min_mz, max_mz, total_intensities):
+        self.clust_no = len(min_mz)
+        self._spec = np.zeros(dtype = float,
+                              shape = (self.clust_no*2,))
+        # real spectrum
+        self._spec[0::2] = min_mz
+        self._spec[1::2] = max_mz
+        self.intensity = total_intensities
+
+    def __getitem__(self, key):
+        i = bisect(self._spec, key)
+        i_div, i_mod = divmod(i, 2)
+        return i_div if i_mod else -1
+
+    def __repr__(self):
+        return self._spec.__repr__()
+
+    def min_max_mz(self, i):
+        return tuple(self._spec[(2*i):(2*i+2)])
+
+
+
+class SpectralIntervals(LightweightSpectrum):
+    def __init__(self, left_mz, right_mz):
+        """Quicker and vectorized version of LightweightSpectrum.
+    
+        Args:
+            left_mz (np.array): left ends of the intervals
+            right_mz (np.array): right ends of the intervals
+        """
+        self.clust_no = len(left_mz)
+        self._spec = np.zeros(dtype = float,
+                              shape = (self.clust_no*2,))
+        # real spectrum
+        self._spec[0::2] = left_mz
+        self._spec[1::2] = right_mz
+
+    def __getitem__(self, mz):
+        w = self._spec
+        i = np.searchsorted(w, mz, side='left')
+        # j = np.searchsorted(w, mz, side='left')
+        # return np.where(i == j, np.floor_divide(i, 2), -1)
+        i_div, i_mod = np.divmod(i, 2)
+        return np.where(i_mod, i_div, -1)
+
+
+
+class OpenIntervals(LightweightSpectrum):
+    def __init__(self, left_mz, right_mz):
+        """Quicker and vectorized version of LightweightSpectrum.
+    
+        Args:
+            left_mz (np.array): left ends of the intervals
+            right_mz (np.array): right ends of the intervals
+        """
+        self.clust_no = len(left_mz)
+        self._spec = np.zeros(dtype = float,
+                              shape = (self.clust_no*2,))
+        # real spectrum
+        self._spec[0::2] = left_mz
+        self._spec[1::2] = right_mz
+
+    def __getitem__(self, mz):
+        il = np.searchsorted(self._spec, mz, side='left')
+        il_div_2, il_mod_2 = np.divmod(il, 2)
+        out = np.where(
+            il_mod,
+            np.where(
+                np.where(
+                    il < len(w),
+                    mz < np.take(self._spec, il, mode='clip'),
+                    False
+                ),
+                il_mod_2,
+                -1
+            ),
+            -1
+        )
+        return out
+
+oi = OpenIntervals([0, 6], [3, 10])
+oi._spec
+oi[mz]
+
+
+
+
+
+
+
+mz = [-1,0,1,2,3]
+w = [0,2]
+il = np.searchsorted(w, mz, side='left')
+ir = np.searchsorted(w, mz, side='right')
+
+mz > np.take(mz, il)
+
+
+il_div_2, il_mod_2 = np.divmod(il, 2)
+ir_div_2, ir_mod_2 = np.divmod(ir, 2)
+
+# (l, r)
+np.where(
+    il_mod,
+    np.where(
+        np.where(
+            il < len(w),
+            mz < np.take(w, il, mode='clip'),
+            False
+        ),
+        il_mod_2,
+        -1
+    ),
+    -1
+)
+
+
+
+
+# [l, r]
+np.where(
+    il_mod,
+    np.where(
+        np.where(
+            il < len(w),
+            mz < np.take(w, il, mode='clip'),
+            False
+        ),
+        il_mod_2,
+        -1
+    ),
+    -1
+)
+
+
+
+
+
+
+np.take([0,2,5], [0,0,0,1])
+
+
+
+i_div, i_mod
+np.where(i_mod, i_div, -1)
+np.isin(mz, w)
+
+mz = np.array([-1, 0, .5, 1, 1.2])
+L = SpectralIntervals([0,5, 8], 
+                      [1,6,10])
+w = L._spec
+mz = np.array([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+L[mz]
+
+
+
+
+
+
+l = np.array([0, 5])
+r = np.array([2, 6])
+
+np.array(list(zip(l,r)))
+
+
+np.searchsorted([-inf, 1,2,3,4,5], 1, side='left')
+
+
+np.searchsorted([-inf, 1,2,3,4,5], 1, side='left')
+
+mz = mz[mz >= 0]
+def classify(mz, w):
+    i = np.searchsorted(w, mz, side='left')
+    i_div, i_mod = np.divmod(i, 2)
+    return np.where(i_mod, i_div, -1)
+
+np.where(mz < w[0], -1, classify(mz, w))
+
+
+np.searchsorted(w, mz, side='left')
+
+
+mz = np.array([0, .5, 1, 1.5, 4, 5, 5.5, 6, 10])
+
+
+def lightweight_spectrum(min_mz, max_mz, total_intensities):
+    return LightweightSpectrum(min_mz,
+                               max_mz,
+                               total_intensities)
+
+
+def test_lightweight_spectrum():
+    L = lightweight_spectrum([0,5,8], 
+                             [1,6,9],
+                             [3,4,5])
+    assert L[-2] == -1
+    assert L[0]  ==  0
+    assert L[.5] ==  0
+    assert L[1]  == -1
+    assert L[1.5]== -1
+    assert L[4]  == -1
+    assert L[5]  ==  1
+    assert L[5.5]==  1
+    assert L[6]  == -1
+    assert L[10] == -1
--- rta/dev_quicker_clusty.py
+++ rta/dev_quicker_clusty.py
@@ -25,7 +25,7 @@ A['signal2medoid_d'] = np.abs(A[['massa_d', 'rta_d', 'dta_d']]).max(axis=1)
 
 AW = A[['id','run','rt']].pivot(index='id', columns='run', values='rt')
 AW = A[['id','run','rt']].pivot(index='id', columns='run', values='rt')
-100 * AW.isnull().values.sum() / np.prod(AW.shape) # 80% of slots are free!
+# 100 * AW.isnull().values.sum() / np.prod(AW.shape) # 80% of slots are free!
 # more than one million free slots
 
 # prepare data for hdbscan
@@ -33,10 +33,10 @@ variables = ['rta', 'dta', 'massa']
 
 ## simplify to peptide-medoids
 Aid = A.groupby('id')
-A_agg = pd.concat(	[Aid[variables].median(),
-					 pd.DataFrame(Aid.size(), columns=['signal_cnt']),
-					 Aid.run.agg(frozenset)],
-					 axis = 1)
+A_agg = pd.concat(  [Aid[variables].median(),
+                     pd.DataFrame(Aid.size(), columns=['signal_cnt']),
+                     Aid.run.agg(frozenset)],
+                     axis = 1)
 # counts = Counter(A_agg.signal_cnt)    # number of peptides that occur a given number of times
 # [(k, k*v) for k, v in counts.items()] # number of signals
 
@@ -57,16 +57,16 @@ a_agg_no_fulls = A_agg_no_fulls.iloc[[0,1,2,3]]
 x = a_agg_no_fulls.iloc[0]
 
 def nn_iter(x):
-	peptID, p_rta, p_dta, p_mass, p_run_cnt, p_runs = x
-	for r in runs:
-		if not r in p_runs:
-			d, idx = F[r].query([p_mass, p_rta, p_dta], p=inf, k=1)
-			nn_mass, nn_rta, nn_dta = F[r].data[idx]
-			yield (peptID, r, nn_mass, nn_rta, nn_dta, idx, d)
+    peptID, p_rta, p_dta, p_mass, p_run_cnt, p_runs = x
+    for r in runs:
+        if not r in p_runs:
+            d, idx = F[r].query([p_mass, p_rta, p_dta], p=inf, k=1)
+            nn_mass, nn_rta, nn_dta = F[r].data[idx]
+            yield (peptID, r, nn_mass, nn_rta, nn_dta, idx, d)
 
 def fill_iter(X):
-	for x in X.values:
-		yield from nn_iter(x)
+    for x in X.values:
+        yield from nn_iter(x)
 # %%time
 # FILLED = pd.DataFrame(fill_iter(A_agg_no_fulls))
 # variables = ['id', 'run', 'massa', 'rta', 'dta', 'idx', 'd']
@@ -80,53 +80,135 @@ def fill_iter(X):
 # A_['origin'] = 'A'
 # ALL = pd.concat([A_, FILLED], axis=0, sort=False)
 # ALL.to_msgpack(data/"nnn_zlib.msg", compress='zlib') # naive nearest neighbours
-A_agg = A_agg.sort_values('massa')
-sum(np.diff(A_agg.massa) > .1)
-
-
+# A_agg = A_agg.sort_values('massa')
+# sum(np.diff(A_agg.massa) > .1)
 
 # define few meaningful splits: based on mmu (mili-mass units)
-thr = '5mmu'
-parse_thr(thr)
-thr = '5da'
-parse_thr(thr)
-thr = '5ppm'
-parse_thr(thr)
+# thr = '5mmu'
+# parse_thr(thr)
+# thr = '5da'
+# parse_thr(thr)
+# thr = '5ppm'
+# parse_thr(thr)
 
-w = A.groupby('id').massa_d.median()
-w = w[w > 0]
-sum(np.diff(A_agg.massa) > max(w))
+# w = A.groupby('id').massa_d.median()
+# w = w[w > 0]
+# sum(np.diff(A_agg.massa) > max(w))
 
-x = A_agg.massa.values
-max_allowed = 0.001
-w = np.arange(len(x)-1)[np.diff(x) > max_allowed]
+# x = A_agg.massa.values
+# max_allowed = 0.001
+# w = np.arange(len(x)-1)[np.diff(x) > max_allowed]
 
-res = {}
-for r in runs:
-	res[r] = F[r].query(
-	 	A_agg.loc[A_agg.run.apply(lambda x: r not in x), variables],
-		p=inf,
-		k=1)
+# res = {}
+# for r in runs:
+#     res[r] = F[r].query(
+#         A_agg.loc[A_agg.run.apply(lambda x: r not in x), variables],
+#         p=inf,
+#         k=1)
 # to do: check for balls instead
 # what is this box-size? Maybe it's precisely the thing I look for?
 # make test
 # also, maybe it is still a good idea to get the closest points in all directions
 # to somehow estimate the noise level? No, this is to vague
 
-
 from rta.array_operations.dataframe_ops import get_hyperboxes
 
 Did = D.groupby('id')
-HB = pd.concat(	[	get_hyperboxes(D, variables),
-					Did[variables].median(),
-					pd.DataFrame(Did.size(), columns=['signal_cnt']),
-				 	Did.run.agg(frozenset)],
-					axis = 1)
+HB = pd.concat( [   get_hyperboxes(D, variables),
+                    Did[variables].median(),
+                    pd.DataFrame(Did.size(), columns=['signal_cnt']),
+                    Did.run.agg(frozenset),
+                    Did.charge.median(),
+                    Did.FWHM.median()     ],
+                    axis = 1)
 HB = HB[HB.signal_cnt > 5]
+
+plt.hexbin(HB.rta_min, HB.rta_edge)
+plt.hexbin(np.log(HB.rta_min), np.log(HB.rta_edge))
+plt.show()
+plt.hexbin(HB.dta_min, HB.dta_edge)
+plt.hexbin(np.log(HB.dta_min), np.log(HB.dta_edge))
+plt.show()
+(ggplot(HB, aes(x='dta_min', y='dta_edge')) +
+    geom_density_2d() +
+    facet_wrap('charge'))
+# (ggplot(HB, aes(x='FWHM', y='dta_edge')) +
+#   geom_density_2d() +
+#   facet_wrap('charge'))
+(ggplot(HB, aes(x='massa_min', y='massa_edge')) +
+    geom_density_2d())
+(ggplot(HB, aes(x='massa_min', y='massa_edge')) +
+    geom_density_2d() + facet_wrap('signal_cnt'))
+(ggplot(HB, aes(x='massa_min', y='massa_edge')) +
+    geom_density_2d() +
+    facet_wrap('charge'))
+(ggplot(HB, aes(x='massa_edge', group='charge', color='charge')) +
+    geom_density())
+
+# all values have been filtered so that only one charge state is used for the analysis
+Counter(A.groupby('id').charge.nunique())
+
+# %%time
+# F = kd_tree(U[variables])
+## 36.7 seconds
+
+# %%time
+# F = {run: kd_tree(U.loc[U.run == run, variables]) for run in runs}
+# ## 5.15 seconds
+
+charges = np.array(list(set(U.charge.unique()) | set(A.charge.unique())))
+
+# %%time
+# F = {}
+# U_var = U.loc[:,variables]
+# for q in charges:
+#     for r in runs:
+#         row_select = np.logical_and(U.run == r, U.charge == q)
+#         F[(r,q)] = U_var.loc[row_select,:] if np.any(row_select) else None
+# # 3.01 sec
+
+%%time
+F = {}
+U_var = U.loc[:,variables]
+for x in U[['run', 'charge']].drop_duplicates().itertuples():
+    r, q = x.run, x.charge
+    row_select = np.logical_and(U.run == r, U.charge == q)
+    F[(r,q)] = kd_tree(U_var.loc[row_select,:])
+# 2.44 sec: twice faster
+# OK, the more subselection, the faster the construction of the kd-tree
+
+#### GREAT!!!! So lets divide it all by the masses!!!! GREAT!!!
+M = U.loc[np.logical_and(U.run == 1, U.charge == 2), 'massa'].values
+
+M = np.sort(M)
+dM = np.diff(M)
+sum(dM > .1)
+
+
+L = M - .1
+R = M + .1
+
+
+
+
+plt.hist(np.log(dM[dM > 0]), bins=100)
+plt.show()
+
+w = w[w > 0]
+sum(np.diff(A_agg.massa) > max(w))
+U.sort_values(['run', 'mass'])
+
+
+
+
+
+
+
+
+
 
 # HB_long = pd.melt(HB[[v+'_edge' for v in variables]])
 HB_long = pd.melt(HB[['signal_cnt', 'rta_edge', 'dta_edge', 'massa_edge']], id_vars='signal_cnt')
-
 
 np.percentile(HB.rta_edge/np.percentile(HB.rta_edge, .99), .95)
 
@@ -135,15 +217,33 @@ lim_rect = HB[['rta_edge', 'dta_edge', 'massa_edge']].apply(lambda x: np.percent
 W = HB[['rta_edge', 'dta_edge', 'massa_edge']]/lim_rect
 X = pd.melt(W)
 
+# these look more or less independent: but maybe an analysis of some other metric would be more sensible?
+(ggplot(HB, aes(x='dta_edge', y='rta_edge')) + 
+    geom_density_2d() + 
+    coord_fixed() + 
+    scale_y_log10() +
+    scale_x_log10())
 
+(ggplot(HB, aes(x='massa_edge', y='rta_edge')) + 
+    geom_density_2d() + 
+    coord_fixed() + 
+    scale_y_log10() +
+    scale_x_log10())
+
+(ggplot(HB, aes(x='massa_edge', y='dta_edge')) + 
+    geom_density_2d() + 
+    coord_fixed() + 
+    scale_y_log10() +
+    scale_x_log10())
 
 (ggplot(X, aes(x='value', color='variable', group='variable')) + geom_density())
+
+
 
 # applying the normalization for test.
 lim_rect.index = pd.Index(variables)
 variables_n = [v+'_n' for v in variables]
 U[variables_n] = U[variables]/lim_rect
-A
 
 
 
